6cd706f7253b568d21bd128bdf2162f2e3cf77ae
spencerwooo <spencerwoo98@gmail.com>
2019-04-27 23:32:13 +0800

    Add device list: device details list view

--- src/main/java/com/spencer/PrimaryController.java
+++ src/main/java/com/spencer/PrimaryController.java
@@ -17,12 +17,24 @@ import java.util.List;
 
 public class PrimaryController {
 
-    private static Scene scene;
-
     @FXML
     private ListView<String> deviceListView;
     @FXML
-    private Label selectedDeviceLabel;
+    private Label deviceNameLabel;
+    @FXML
+    private Label deviceDescriptionLabel;
+    @FXML
+    private Label deviceAddressLabel;
+    @FXML
+    private Label deviceLinkLayerAddressLabel;
+    @FXML
+    private Label deviceLoopBackLabel;
+    @FXML
+    private Label deviceUpLabel;
+    @FXML
+    private Label deviceRunningLabel;
+    @FXML
+    private Label deviceLocalLabel;
 
     private ArrayList<String> listViewData = new ArrayList<>();
     private ObservableList<String> observableList = FXCollections.observableArrayList();
@@ -41,7 +53,18 @@ public class PrimaryController {
 
         deviceListView.getSelectionModel().selectedItemProperty().addListener((observableValue, s, t1) -> {
             int index = deviceListView.getSelectionModel().getSelectedIndex();
-            selectedDeviceLabel.setText(allDevices.get(index).toString());
+            PcapNetworkInterface selectedDevice = allDevices.get(index);
+
+            System.out.println(selectedDevice.toString());
+
+            deviceNameLabel.setText(selectedDevice.getName());
+            deviceDescriptionLabel.setText(selectedDevice.getDescription());
+            deviceAddressLabel.setText(selectedDevice.getAddresses().toString());
+            deviceLinkLayerAddressLabel.setText(selectedDevice.getLinkLayerAddresses().toString());
+            deviceLoopBackLabel.setText(Boolean.toString(selectedDevice.isLoopBack()));
+            deviceUpLabel.setText(Boolean.toString(selectedDevice.isUp()));
+            deviceRunningLabel.setText(Boolean.toString(selectedDevice.isRunning()));
+            deviceLocalLabel.setText(Boolean.toString(selectedDevice.isLocal()));
         });
     }
 
@@ -65,7 +88,7 @@ public class PrimaryController {
             SecondaryController secondaryController = loader.getController();
             secondaryController.setDevice(selectedDevice);
 
-            Scene scene = selectedDeviceLabel.getScene();
+            Scene scene = deviceNameLabel.getScene();
             scene.setRoot(parent);
         }
 
